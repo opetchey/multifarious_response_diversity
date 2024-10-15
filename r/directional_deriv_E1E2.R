6ec3b6c3e8de2bf0f6175907ec4d8fd645261d3f
Francesco Polazzo <111755648+FrancescoPola@users.noreply.github.com>
2024-10-15 11:34:43 +0200

    Revision

--- r/directional_deriv_E1E2.R
+++
@@ -1,5 +0,0 @@
-
-
-directional_deriv <- function(E1_p, E2_p, partial_deriv_E1, partial_deriv_E2){
-directional_derivative =  (partial_deriv_E1 * (E1_p/(sqrt(E1_p^2 + E2_p^2)))) + ((partial_deriv_E2) * (E2_p/(sqrt(E1_p^2 + E2_p^2))))
-}
